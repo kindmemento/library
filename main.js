173ef84aca9dd4b47d04d581e3163117b0142302
kindmemento <kindmemento@gmail.com>
2020-12-12 22:41:51 +0300

    finalized / version 1.0

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function closeForm () {
   form.style.display = 'none'
 }
 
-document.addEventListener('keydown', function(event){
+document.addEventListener('keydown', function(event) {
 	if(event.key === "Escape"){
 		form.style.display = 'none'
 	}
@@ -26,6 +26,7 @@ let myLibrary = []
 function Book() {
   this.title = document.querySelector('#title').value;
   this.author = document.querySelector('#author').value;
+  this.completedPages = document.querySelector('#completed-pages').value
   this.pages = document.querySelector('#pages').value;
 }
 
@@ -42,29 +43,32 @@ function addBookToLibrary() {
   contentContainer.classList.add('container')
   bookCard.appendChild(contentContainer)
 
-  const header = document.createElement('header')
-  contentContainer.appendChild(header)
-
-  const edit = document.createElement('button')
-  edit.innerText = 'Edit'
-  edit.classList.add('add-btn')
-  header.appendChild(edit)
-
-  const remove = document.createElement('button')
-  remove.innerText = 'Remove'
-  remove.classList.add('cancel-btn')
-  header.appendChild(remove)
-
   const title = document.createElement('div')
-  title.innerText = book.title
-  title.classList.add('.title')
+  const titleCapitalized = book.title.charAt(0).toUpperCase() + book.title.slice(1)
+  title.innerText = titleCapitalized
+  title.classList.add('title')
   contentContainer.appendChild(title);
 
   const author = document.createElement('div')
-  author.innerText = book.author
+  const authorCapitalized = book.author.charAt(0).toUpperCase() + book.author.slice(1)
+  author.innerText = 'by ' + authorCapitalized
+  author.classList.add('author')
   contentContainer.appendChild(author)
 
   const pages = document.createElement('div')
-  pages.innerText = book.pages
+  pages.innerText = book.completedPages + '/' + book.pages + ' pages read'
+  pages.classList.add('pages')
   contentContainer.appendChild(pages)
+
+  const footer = document.createElement('footer')
+  contentContainer.appendChild(footer)
+
+  const remove = document.createElement('button')
+  remove.innerText = 'Remove'
+  remove.classList.add('remove-btn')
+  footer.appendChild(remove)
+
+  remove.onclick = function () {
+  library.removeChild(bookCard)
+  }
 }
